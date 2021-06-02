6b3f15da0bb236522bda2618641cbdb4bec2c7e7
ArjunK01 <arjunkumar01@gmail.com>
2021-06-02 12:11:01 -0400

    fix

--- App.js
+++ App.js
@@ -2,9 +2,5 @@ import * as React from "react";
 import Navigation from "./navigation/Navigation";
 
 export default function App() {
-  return (
-    <AuthProvider>
-      <Navigation />
-    </AuthProvider>
-  );
+  return <Navigation />;
 }
